5350419435ab7437ff3ea6c40ab39ee8e5a32515
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2023-01-30 09:43:42 -0300

    Commit

---
+++ src/trees/TreeTest.java
@@ -0,0 +1,26 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeTest {
+    public static void main(String[] args) {
+
+        Tree intTree = new Tree();
+
+        intTree.insert(25);
+        intTree.insert(20);
+        intTree.insert(15);
+        intTree.insert(27);
+        intTree.insert(30);
+        intTree.insert(29);
+        intTree.insert(26);
+        intTree.insert(22);
+        intTree.insert(32);
+
+        intTree.traverseInOrder();
+
+    }
+}
